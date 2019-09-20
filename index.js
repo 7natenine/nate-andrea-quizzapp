f5fd3e6c15a8b3fad821c6fd73f0f751f62ff0b7
Andrea Bender <andreab.1726@gmail.com>
2019-09-20 13:48:08 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Question array
+const STORE = [
+    {
+        question: 'I don’t know how many years I got left on this planet, I’m going to get real weird with it',
+        answers: ['Frank Reynolds', 
+        'Mac',
+        'The Waitress',
+        'Dennis'
+        ],
+        correctAnswer: 'Frank Reynolds'
+    },
+    {
+        question: 'What was Dee\'s nickname in high school?',
+        answers: ['Dumb Dee Dee',
+        'Toothpick',
+        'Aluminum Monster',
+        'Peaches'
+        ],
+        correctAnswer: 'Aluminum Monster' 
+    },
+]
