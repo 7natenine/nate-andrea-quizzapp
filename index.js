7cc80bd79b6670033b1a0c3182727ebc18cebb6b
Nathan Leung <nath.leung@gmail.com>
2019-09-20 13:47:07 -0700

    semi-complete html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Question array
 const STORE = [
     {
-        question: 'I don’t know how many years I got left on this planet, I’m going to get real weird with it',
+        question: 'Which character said: \'I don’t know how many years I got left on this planet, I’m going to get real weird with it\'',
         answers: ['Frank Reynolds', 
         'Mac',
         'The Waitress',
@@ -18,4 +18,78 @@ const STORE = [
         ],
         correctAnswer: 'Aluminum Monster' 
     },
-]
+    {
+        question: 'The thunder of my vengeance will echo through these corridors like the gusts of a thousand winds!',
+        answers: [
+            'Artemis', 
+            'Charlie’s mom', 
+            'Frank', 
+            'Dennis'
+        ],
+        correctAnswer: 'Dennis'
+    },
+    {
+        question: 'What restaurant does Mac repeatedly try to use his Dave and Buster\'s power card at?',
+        answers: [
+            'TGIF', 
+            'Denny\’s', 
+            'Ihop', 
+            Red Robin 
+
+        ],
+    },
+    {
+        questions: 'Which character said: \'I\’m not fat, I’m cultivating mass\'',
+        answers: [
+            'Charlie',
+            'Frank', 
+            'Dee', 
+            'Mac'
+        ],
+    },
+    {
+        questions: 'What does Frank claim his nickname was back when he was a boxer?',
+        answers: [
+            'Frank the Tank',
+            'Frankie Ali', 
+            'Frankie Fast Hands', 
+            'Butterfly Hands'
+        ],
+    },
+    {
+        questions: 'Which character does not appear in season one?',
+        answers: [
+            'The waitress', 
+            'Frank', 
+            'Dee', 
+            'Mac',
+        ],
+    },
+    {
+        questions: 'Which of these does Charlie consider one of his dislikes?',
+        answers: [
+            'People’s knees',
+            'Lobster tails', 
+            'Helmets', 
+            'Egg Shells'
+        ],
+    },
+    {
+        questions: 'What is Mac’s first name?',
+        answers: [
+            'Ronald', 
+            'Conrad', 
+            'Benjamin', 
+            'Frank'
+        ],
+    },
+    {
+        questions: 'What names do Dennis and Mac give themselves when they\'re posing as realtors?',
+        answers: [
+            'Honey and Vinegar', 
+            'Sugar and Spice', 
+            'Jekyll and Hide', 
+            'Lemon and Lime'
+        ],
+    }
+];
